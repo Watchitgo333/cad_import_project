fc5d2ea0a451886575ca8815c5b6f657d40e537b
Eric Brusky <bruskyeric@gmail.com>
2023-03-08 11:53:15 -0600

    added script to create reference planes from cad lines

--- test_scripts/compiled_test_scripts.py
+++ test_scripts/compiled_test_scripts.py
@@ -310,6 +310,44 @@ fec = FilteredElementCollector(doc, doc.ActiveView.Id)
 fec2 = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_CLines)
 c = doc.Settings.Categories.get_Item(BuiltInCategory.OST_Lines)
 
+#sub = c.SubCategories
+
+# for line in sub:
+# 	if line.Name.Contains("ME-WALL"):
+# 		me_wall = line
+		#print(me_wall)
+geo = []
+eles = []
+for ele in fec:
+	if ele.GetType().Equals(ModelLine):
+		geo.append(ele.get_Geometry(op))
+		eles.append(ele)
+		#print(ele.get_Geometry(op).GetType())
+	
+g = geo[0]
+e = eles[0]
+#print(g, e)
+
+for ln in eles:
+	print(ln.LineStyle.Name)
+	if ln.LineStyle.Name == "ME-WALL":
+		t.Start("Curve")
+		doc.Create.NewDetailCurve(doc.ActiveView, ln.GeometryCurve)
+		t.Commit()
+
+#Creates ref plane along a cad line
+from Autodesk.Revit.DB import Transaction, XYZ, BuiltInParameter, BuiltInCategory, FilteredElementCollector, ImportInstance
+import Autodesk.Revit.DB as DB
+doc = __revit__.ActiveUIDocument.Document
+op = doc.Application.Create.NewGeometryOptions()
+op.ComputeReferences = True
+op.IncludeNonVisibleObjects = True
+t = Transaction(doc)
+
+fec = FilteredElementCollector(doc, doc.ActiveView.Id)
+fec2 = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_CLines)
+c = doc.Settings.Categories.get_Item(BuiltInCategory.OST_Lines)
+
 sub = c.SubCategories
 
 for line in sub:
@@ -327,9 +365,17 @@ for ele in fec:
 g = geo[0]
 e = eles[0]
 #print(g, e)
-
+me_walls = []
 for ln in eles:
-	print(ln.GeometryCurve)
-	t.Start("Curve")
-	doc.Create.NewDetailCurve(doc.ActiveView, ln.GeometryCurve)
-	t.Commit()
+	if ln.LineStyle.Name == "ME-WALL":
+		me_walls.append(ln)
+print(me_walls[0].GeometryCurve.GetEndPoint(0))
+
+ref1 = me_walls[0].GeometryCurve.GetEndPoint(0)
+ref2 = me_walls[1].GeometryCurve.GetEndPoint(1)
+t.Start("Ref")
+doc.Create.NewReferencePlane(ref1, ref2, XYZ(0,0,-1), doc.ActiveView)
+t.Commit()
+# t.Start("Ref")
+# doc.Create.NewReferencePlane(XYZ(-22.6588644220049, 11.0704374588559, 0), XYZ(-47.3671977553383, 11.0704374588559, 0), XYZ(0,0,-1), doc.ActiveView)
+# t.Commit()
